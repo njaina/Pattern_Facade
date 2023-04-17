cd14e2f100f9bba13cd03154c55f825f4e2f9baf
njaina <hei.njaina.2@gmail.com>
2023-04-17 16:29:28 +0300

    feat: init project

---
+++ src/main/java/com/example/FacadeTraining/ConcreteDependace2.java
@@ -0,0 +1,4 @@
+package com.example.FacadeTraining;
+
+public class ConcreteDependace2 {
+}
---
+++ src/test/java/com/example/FacadeTraining/FacadeTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FacadeTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
